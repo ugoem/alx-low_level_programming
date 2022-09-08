e6efa22c291bcbbbaaa7649064edf81d88411583
Anionovo Ugochukwu E <esuneteng.tech@gmail.com>
2022-09-08 22:05:09 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
-/**
- * This C program that prints exactly - (Programming is like building a multilingual puzzle), followed by a new line.
- */
- 
 #include <stdio.h>
+
+/**
+ * This C program that prints exactly - (Programming is like building a multilingual puzzle), 
+ * followed by a new line.
+ * Returns 0 if exited properly, nom-zero otherwise
+ */
 
 int main (void)
 {
