077f667c95a4c5dd630b2ff28abe9cf5763c0992
Anionovo Ugochukwu E <esuneteng.tech@gmail.com>
2022-09-08 22:35:10 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main -prints exactly with proper grammar, 
+ * main -prints exactly with proper grammar,
  * but the outcome is a piece of art,
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
-{	
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
