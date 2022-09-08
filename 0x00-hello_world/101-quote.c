1b2ee43f280d8d04dc8d012ba5c915a661ecf21b
Anionovo Ugochukwu E <esuneteng.tech@gmail.com>
2022-09-08 22:59:58 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,10 +7,8 @@
  *  followed by a new line, to the standard error.
  *  Returns: always 0 (Success)
  */
-
 int main(void)
 {
-	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59);
-	return(1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
-
