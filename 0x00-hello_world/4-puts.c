16b132f649b9135a03f6dba0ba21bfb502b023ea
Anionovo Ugochukwu E <esuneteng.tech@gmail.com>
2022-09-08 22:07:41 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * This C program that prints exactly - (Programming is like building a multilingual puzzle), 
+ * main- This C program that prints exactly - (Programming is like building a multilingual puzzle), 
  * followed by a new line.
- * Returns 0 if exited properly, nom-zero otherwise
+ * Returns: 0 if exited properly, nom-zero otherwise
  */
 
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
