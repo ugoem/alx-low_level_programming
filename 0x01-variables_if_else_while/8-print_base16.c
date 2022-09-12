8d34dc41ea1c094afcf8c3f767e1fc1f7b2dfa4b
Anionovo Ugochukwu E <esuneteng.tech@gmail.com>
2022-09-12 14:38:54 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - prints all the numbers of base 16 in lowercase, 
+ * main - prints all the numbers of base 16 in lowercase,
  * followed by a new line
  * Return: (0)
  */
