667debb3392cfbced5c4a50e1687851ac99034cd
Anionovo Ugochukwu E <esuneteng.tech@gmail.com>
2022-09-12 15:22:53 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,17 +9,17 @@ int main(void)
 {
 	int n1, n2, n3;
 
-	for (n1 = 48; n1 < 58; n1++) 
+	for (n1 = 48; n1 < 58; n1++)
 	{
-		 for (n2 = 49; n2 < 58; n2++)
-		 {
-			  for (n3 = 50; n3 < 58; n3++)
-			  {
-				 if (n3 > n2 && n2 > n1)
-				 {
-					 putchar(n1);
-			  		 putchar(n2);
-					 putchar(n3);
+		for (n2 = 49; n2 < 58; n2++)
+		{
+			for (n3 = 50; n3 < 58; n3++)
+			{
+				if (n3 > n2 && n2 > n1)
+				{
+					putchar(n1);
+					putchar(n2);
+					putchar(n3);
 					if (n1 != 55 || n2 != 56)
 					{
 						putchar(',');
