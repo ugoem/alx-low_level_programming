60012112e3e8daf27bc11987e0a2c81390ee5ebb
Anionovo Ugochukwu E <esuneteng.tech@gmail.com>
2022-09-12 14:36:35 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,15 +3,15 @@
 
 /**
  * main - prints the lowercase alphabet in reverse, followed by a new line.
- * Return (0)
+ * Return: (0)
  */
 int main(void)
 {
 	int revlower;
-	
+
 	for (revlower = 'z'; revlower >= 'a'; revlower--)
 	{
-		putchar(revlower); 
+		putchar(revlower);
 	}
 	putchar('\n');
 	return (0);
