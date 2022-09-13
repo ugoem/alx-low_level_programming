c095a8de320755a23b3bd988b2df4a3140d88488
ugoem <esuneteng.tech@gmail.com>
2022-09-13 22:10:38 +0000

    Commit all in task 3

--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -3,10 +3,24 @@
 /**
  * main - checks the code
  *
- * Return: 0 Always if success
+ * Return: 1 if c is lowercase, 0 otherwise
  */
 int main(void)
 {
-	print_alphabet_x10();
+	int _islower(int c);
+
+	int r;
+    
+	r = _islower('m');
+	_putchar(r + '0');
+	r = _islower('A');
+	_putchar(r + '0');
+	r = _islower(108);
+	 _putchar(r + '0');
+	r = _islower('i');
+	_putchar(r + '0');
+	r = _islower('N');
+	_putchar(r + '0');
+	
 	return (0);
 }
