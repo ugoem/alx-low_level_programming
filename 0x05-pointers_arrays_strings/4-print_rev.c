a0d14877bc32bbddeca112580f23c3fc82a3cb5d
Anionovo Ugochukwu E <esuneteng.tech@gmail.com>
2022-09-19 19:19:13 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,7 +2,7 @@
 
 /**
  * print_rev - prints a string, in reverse, followed by a new line.
- * @*s: string to be printed in reverse order
+ * @s: string to be printed in reverse order
  * Return: string
  */
 void print_rev(char *s)
