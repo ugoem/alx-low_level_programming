d9b2e7bb17714fce82f1210dff5435463cde1e86
Anionovo Ugochukwu E <esuneteng.tech@gmail.com>
2022-09-28 12:23:00 +0100

    create task 0 to 8

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int actual_prime(int n, int k);
+
+/**
+ * is_prime_number - says if an integer is a prime number or not
+ * @n: number to evaluate
+ *
+ * Return: 1 if n is a prime number, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n - 1));
+}
+
+/**
+ * actual_prime - calculates if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+int actual_prime(int n, int k)
+{
+	if (k == 1)
+		return (1);
+	if (n % k == 0 && k > 0)
+		return (0);
+	return (actual_prime(n, k - 1));
+}
