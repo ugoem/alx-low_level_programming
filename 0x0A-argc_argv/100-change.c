7c78f003b924bcf6e9446fda92fb85f7b3595735
Anionovo Ugochukwu E <esuneteng.tech@gmail.com>
2022-09-30 18:01:22 +0100

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,6 +3,45 @@
 #include "main.h"
 
 /**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int atoi(char *s)
+{
+
+	int i, d, n, ln, f, dgt;
+
+	i = d = n = ln = f = dgt = 0;
+
+	while (s[ln] != '\0')
+		ln++;
+	
+	while (i < ln && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+		
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			dgt = s[i] - '0';
+			if (d % 2)
+				dgt = -dgt;
+			n = n * 10 + dgt;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+		return (0);
+	return (n);
+}
+
+/**
  * main - prints the minimum number of coins to
  * make change for an amount of money
  * @argc: number of arguments
