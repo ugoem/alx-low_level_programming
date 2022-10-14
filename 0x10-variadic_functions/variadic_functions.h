680c406d1475d7efa20a58baf832509050ac51ff
Anionovo Ugochukwu E <esuneteng.tech@gmail.com>
2022-10-14 08:30:51 +0100

    Update header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,13 +7,9 @@
  * Desc: Header file containing prototypes for all functions
  *       used in the 0x0F-variadic_functions directory.
  */
-
-#include <stdarg.h>
-
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
 #endif /*Variadic_function*/
